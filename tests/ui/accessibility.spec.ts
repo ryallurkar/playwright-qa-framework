a9c4282569cfe3f4f86239add31eec823d6185b8
Raj <raju90y@outlook.com>
2026-03-03 15:46:37 +0100

    Add accessibility example

---
+++ tests/ui/accessibility.spec.ts
@@ -0,0 +1,20 @@
+import AxeBuilder from '@axe-core/playwright';
+import { test, expect } from '@fixtures/customFixtures';
+
+test.describe('Saucedemo Login Accessibility', () => {
+  test('should have no critical accessibility violations on the login page @smoke @ui', async ({
+    loginPage,
+    page,
+  }) => {
+    await test.step('Arrange', async () => {
+      await loginPage.goto();
+      await expect(await loginPage.isLoginPageVisible()).toBe(true);
+    });
+
+    await test.step('Assert', async () => {
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  });
+});
